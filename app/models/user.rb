2358aaf6bfe7faf7a13fdbb72e5e6336bf83f306
David Hernandez <d.hdza19@gmail.com>
2024-05-08 15:52:17 -0600

    feat: improve ui for login and signup page

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  devise :database_authenticatable, :registerable, :validatable, :rememberable
 
   has_many :families, dependent: :destroy, inverse_of: :user
   has_many :members, dependent: :destroy, inverse_of: :user
